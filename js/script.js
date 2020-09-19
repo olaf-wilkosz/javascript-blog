00f2fde59f745ac004f6bbd28de065d8f10eaeb5
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-09-19 22:45:35 +0200

    Add getting the article id from clicked link and adding class active to chosen article to script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 }); */
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
@@ -29,11 +30,20 @@ const titleClickHandler = function(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector)
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+
+  console.log('targetArticle:', targetArticle);
+  
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
