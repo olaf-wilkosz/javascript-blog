f53e513af1f0bee903d58f73c24f4d2bb99469a3
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-09-25 01:18:33 +0200

    Add to generateTags code to generate the tag list for all the tags of articles

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
+    optTagsListSelector = '.tag.list';
 
   function titleClickHandler(event) {
     event.preventDefault();
@@ -89,56 +90,60 @@
   generateTitleLinks();
 
   function generateTags() {
-    /* find all articles */
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
 
+    /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
 
     /* START LOOP: for every article: */
-
     for (let article of articles) {
 
       /* find tags wrapper */
-
       const tagsWrapper = article.querySelector(optArticleTagsSelector);
       tagsWrapper.innerHTML = '';
 
-
       /* make html variable with empty string */
-
       let html = '';
 
       /* get tags from data-tags attribute */
-
       const articleTags = article.getAttribute('data-tags');
 
       /* split tags into array */
-
       const articleTagsArray = articleTags.split(' ');
 
       /* START LOOP: for each tag */
-
       for (let tag of articleTagsArray) {
 
-
         /* generate HTML of the link */
-
         const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li> ';
 
         /* add generated code to html variable */
-
         html = html + linkHTML;
+
+        /* [NEW] check if this link is NOT already in allTags */
+        if(allTags.indexOf(linkHTML) == -1){
+
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
 
       }
 
       /* END LOOP: for each tag */
 
       /* insert HTML of all the links into the tags wrapper */
-
       tagsWrapper.innerHTML = html;
 
     }
 
     /* END LOOP: for every article: */
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
 
   }
 
