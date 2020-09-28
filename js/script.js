86827f1fda5e0a59d15863a7841f1506b2fd5a3e
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-09-28 20:31:37 +0200

    Add generating tag cloud from tags and styling according to the number of appearances

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
     optArticleAuthorSelector = '.post-author',
-    optTagsListSelector = '.tag.list';
+    optTagsListSelector = '.tag.list',
+    optCloudClassCount = 5,
+    optCloudClassPrefix = 'tag-size-';
 
   function titleClickHandler(event) {
     event.preventDefault();
@@ -77,6 +79,34 @@
 
   generateTitleLinks();
 
+  function calculateTagsParams(tags) {
+    const params = { max: 0, min: 999999 };
+
+    for (let tag in tags) {
+      console.log(tag + ' is used ' + tags[tag] + ' times');
+
+      if (tags[tag] > params.max) {
+        params.max = tags[tag];
+      }
+
+      if (tags[tag] < params.min) {
+        params.min = tags[tag];
+      }
+    }
+
+    return params;
+  }
+
+  function calculateTagClass(count, params) {
+
+    const normalizedCount = count - params.min;
+    const normalizedMax = params.max - params.min;
+    const percentage = normalizedCount / normalizedMax;
+    const classNumber = Math.floor(percentage * (optCloudClassCount - 1) + 1);
+
+    return optCloudClassPrefix + classNumber;
+  }
+
   function generateTags() {
 
     /* [NEW] create a new variable allTags with an empty object */
@@ -133,13 +163,17 @@
     /* [NEW] find list of tags in right column */
     const tagList = document.querySelector('.tags');
 
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams);
+
     /* [NEW] create variable for all links HTML code */
     let allTagsHTML = '';
 
     /* [NEW] START LOOP: for each tag in allTags: */
     for (let tag in allTags) {
 
-      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ')</a></li>';
+      const linkHTML = '<li><a href="#tag-' + tag + '" class="' + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + '</a></li> ';
+      console.log('linkHTML:', linkHTML);
 
       /* [NEW] generate code of a link and add it to allTagsHTML */
       allTagsHTML += linkHTML;
