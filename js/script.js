6c2fffffae39713fceb02355d0c7b148fb3a63ff
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-09-29 16:14:26 +0200

    Add Handlebars template to generate the tags links under each article

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
   const templates = {
     articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
     tagLink: Handlebars.compile(document.querySelector('#template-tag-link').innerHTML),
+    authorLink: Handlebars.compile(document.querySelector('#template-author-link').innerHTML)
   };
 
   const opt = {
